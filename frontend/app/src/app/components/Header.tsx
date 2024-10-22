c84b33cbf204c7d80499d7ec87a0591b31581ea5
EnriqueSaracho <90583844+EnriqueSaracho@users.noreply.github.com>
2024-10-22 13:59:31 -0700

    TypeScriptfied my code

--- frontend/app/src/app/components/Header.tsx
+++ frontend/app/src/app/components/Header.tsx
@@ -1,9 +1,10 @@
+import { FC } from "react";
 import Link from "next/link";
 import { IoHome } from "react-icons/io5";
 import Image from "next/image";
 import langaraLogo from "../assets/images/langara-logo.png";
 
-const Header = () => {
+const Header: FC = () => {
   return (
     <div className="fixed top-0 left-0 h-14 w-screen flex justify-between items-center px-4 py-2 z-30 bg-black">
       <Link href="/Menu" className="focus:outline-none rounded-sm">
@@ -18,6 +19,6 @@ const Header = () => {
       />
     </div>
   );
-}
+};
 
 export default Header;
