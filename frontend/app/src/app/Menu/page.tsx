e66b7ad6164b086a62db53c3691a35ce34792a00
EnriqueSaracho <90583844+EnriqueSaracho@users.noreply.github.com>
2024-10-23 18:05:19 -0700

    Refactored Menu page

--- frontend/app/src/app/Menu/page.tsx
+++ frontend/app/src/app/Menu/page.tsx
@@ -1,49 +1,24 @@
 import { FC } from "react";
-import Link from "next/link";
+import MenuLink from "../components/MenuLink";
 
 const Page: FC = () => {
   return (
     <div className="px-8">
       <ul className="flex flex-col justify-center content-start h-screen text-2xl font-medium">
         <li>
-          <Link
-            href={"/Profile"}
-            className="block pt-8 pb-1 pl-1 mt-2 w-60 border-orange-500 border-b hover:bg-neutral-800 focus:outline-none focus:bg-neutral-700 transition-colors duration-200"
-          >
-            Profile
-          </Link>
+          <MenuLink path="/Profile" label="Profile" />
         </li>
         <li>
-          <Link
-            href={"/CourseScheme"}
-            className="block pt-8 pb-1 pl-1 mt-2 w-60 border-orange-500 border-b hover:bg-neutral-800 focus:outline-none focus:bg-neutral-700 transition-colors duration-200"
-          >
-            Courses Scheme
-          </Link>
+          <MenuLink path="/CourseScheme" label="Course Scheme" />
         </li>
         <li>
-          <Link
-            href={"/CourseList"}
-            className="block pt-8 pb-1 pl-1 mt-2 w-60 border-orange-500 border-b hover:bg-neutral-800 focus:outline-none focus:bg-neutral-700 transition-colors duration-200"
-          >
-            Courses List
-          </Link>
+          <MenuLink path="/CourseList" label="Courses List" />
         </li>
         <li>
-          <Link
-            href={"/FacultyContacts"}
-            className="block pt-8 pb-1 pl-1 mt-2 w-60 border-orange-500 border-b hover:bg-neutral-800 focus:outline-none focus:bg-neutral-700 transition-colors duration-200"
-          >
-            Faculty Contacts
-          </Link>
+          <MenuLink path="/FacultyContacts" label="Faculty Contacts" />
         </li>
         <li>
-          <Link
-            href={"/AppGuide"}
-            className="block pt-8 pb-1 pl-1 mt-2 w-60 border-orange-500 border-b hover:bg-neutral-800 focus:outline-none focus:bg-neutral-700 transition-colors duration-200"
-          >
-            App Guide
-          </Link>
+          <MenuLink path="/AppGuide" label="App Guide" />
         </li>
       </ul>
     </div>
